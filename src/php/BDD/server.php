e90eb277b1a46a36cc4b860067fe59b661f55632
The-Nico26 <nicolas.mazard@viacesi.fr>
2017-04-19 11:13:13 +0200

    cc

--- src/php/BDD/server.php
+++ src/php/BDD/server.php
@@ -4,7 +4,7 @@
     	
     	public static function connexion(){
     		try {
-				self::$pdo = new PDO('mysql:host=mysql;dbname=bde;charset=utf8', 'bde', 'cesi');
+				self::$pdo = new PDO('mysql:host=mysql;de=bde;charset=utf8', 'bde', 'cesi');
 				self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			}
 			catch (Exception $e){
